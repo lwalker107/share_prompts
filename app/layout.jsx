b8b2bf1cbe0135131cf4b19683e834d9ea70bbdc
lwalker <landen@example.com>
2023-10-25 19:50:53 -0500

    finished home + layout pages in app folder

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Promptopolis",
+    description: 'Discover and Share AI Prompts'
+}
+const RootLayout = () => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
