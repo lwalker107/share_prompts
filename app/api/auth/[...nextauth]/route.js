f1c9ca436003acd5f5334e9a7351e7b8f0ffe810
lwalker <landen@example.com>
2023-10-27 07:18:43 -0500

    added google auth providers + updated api keys

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,15 +4,20 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
     providers: [
         GoogleProviders({
-            clientId: "",
-            clientSecret: "",
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
 
     },
     async signIn({ profile }) {
+        try {
+            // serverless -> lambda -> dynamodb
+            // only opens up when it gets called and helps prevent from having the server running constantly
+        } catch (error) {
 
+        }
     }
 })
 
