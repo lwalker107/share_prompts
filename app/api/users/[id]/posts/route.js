0c5e1d9e8fdd312085ec25dbe0beb44c3bfd04dc
lwalker <landen@example.com>
2023-11-01 06:59:13 -0500

    created api endpoint for user routes and posts

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,22 @@
+import { connectToDB } from "@utils/database";
+import Prompt from '@models/prompt';
+
+// API route that gets all the prompts
+
+export const GET = async (req, {params}) => {
+    try {
+        await connectToDB();
+
+        const prompts = await Prompt.find({
+            creator: params.id
+        }).populate('creator');
+
+        return new Response(JSON.stringify(prompts), { 
+            status: 200
+        })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { 
+            status: 500
+        })
+    }
+}
