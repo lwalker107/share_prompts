5dc365160e5b3813072c547d128a54d5111474f1
lwalker <landen@example.com>
2023-10-31 20:32:00 -0500

    created new post page that connects to MongoDB server

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,11 +1,23 @@
 import { connectToDB } from "@utils/database";
-// Creating the backend for the app with nextjs
+import Prompt from '@models/prompt';
+// Creating the api route for the app with nextjs
 export const POST = async (req) => {
     const { userId, prompt, tag } = await req.json();
 
     try {
         await connectToDB();
-    } catch (error) {
+        const newPrompt = new Prompt({ 
+            creator: userId, 
+            prompt,
+            tag 
+        })
 
+        await newPrompt.save();
+
+        return new Response(JSON.stringify(newPrompt), { 
+            status: 201
+        })
+    } catch (error) {
+        return new Response("Failed to create a new response", { status: 500 })
     }
 }
--- models/prompt.js
+++ models/prompt.js
@@ -1,1 +1,23 @@
 import mongoose, { Schema, model, models } from 'mongoose';
+// Models created for making new post
+
+const PromptSchema = new Schema({
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    prompt: {
+        type: String,
+        required: [true, 'Prompt is required.'],
+    },
+    tag: {
+        type: String,
+        required: [true, 'Tag is required.'],
+    }
+});
+
+// creates new prompt if it doesn't exist using the promptschema
+
+const Prompt = models.Prompt || model('Prompt', PromptSchema)
+
+export default Prompt;
