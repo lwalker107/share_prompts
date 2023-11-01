003e3b5390e11338a7e51b94a9c642b47fbb6bac
lwalker <landen@example.com>
2023-10-31 21:19:40 -0500

    useEffect hook used to fetch posts for the feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+'use client';
 
-const Feed = () => {
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard'
+
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div>
-      Feed
+    <div className="mt-16 prompt_layout">
+
     </div>
+  )
+}
+
+// Feed component that shows all of the prompts
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState('');
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+  }, [])
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          type="text" 
+          placeholder="Search for a tag or a username" 
+          value={searchText} 
+          onChange={handleSearchChange} 
+          required 
+          className="search_input peer" 
+          />
+      </form>
+
+      <PromptCardList 
+        data={[]} 
+        handleTagClick={() => {
+
+        }}
+      />
+    </section>
   )
 }
 
